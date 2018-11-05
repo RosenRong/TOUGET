485ec86ed13443ba52cd16954ec1c52ecddc9361
Mars-Sea <1195751600@qq.com>
2018-11-05 19:07:56 +0800

    修改宽度

--- app/src/main/java/com/example/s8534/myaitme/Splash.java
+++ app/src/main/java/com/example/s8534/myaitme/Splash.java
@@ -20,7 +20,7 @@ public class Splash extends AppCompatActivity {
             @Override
             public void run() {
                 try{
-                    sleep(2000);//使程序休眠五秒
+                    sleep(2000);//使程序休眠2秒
                     Intent it=new Intent(getApplicationContext(),MainActivity.class);//启动MainActivity
                     startActivity(it);
                     finish();//关闭当前活动
